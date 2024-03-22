357e97743f2e2e4b415c1eb11686804c1a8b1d48
AI_Nomads <chugue85@gmail.com>
2024-03-22 22:42:23 +0900

    board List

--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import shop.mtcoding.blog.user.User;
 
 import java.util.List;
 
@@ -12,6 +13,22 @@ import java.util.List;
 @Repository
 public class BoardRepository {
     private final EntityManager em;
+
+    public List<Board> findAllV2 () {
+        Query q1 = em.createQuery("select b from Board b order by b.id desc", Board.class);
+        List<Board> boardList = q1.getResultList();
+        List<Integer> userIds = boardList.stream().mapToInt(value -> value.getUser().getId()).distinct().boxed().toList();
+
+        Query q2 = em.createQuery("select u from User u where u.id in :userIds", User.class);
+        q2.setParameter("userIds", userIds);
+        List<User> userList = q2.getResultList();
+
+        boardList.stream().forEach(board -> {
+            User user = userList.stream().filter(u -> u.getId() == board.getUser().getId()).findFirst().get();
+            board.setUser(user);
+        });
+        return boardList;
+    }
 
     public Board findByIdJoinUser(int id){
         String q = """
--- src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
@@ -2,10 +2,12 @@ package shop.mtcoding.blog.board;
 
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import shop.mtcoding.blog.user.User;
 
 import java.util.List;
 
@@ -18,7 +20,36 @@ public class BoardRepositoryTest {
     private BoardRepository boardRepository;
 
     @Test
-    public void findAll_test(){
+    public void randomQuery_test() {
+        String q1 = "select b from Board b order by b.id desc";
+        Query query = em.createQuery(q1, Board.class);
+        List<Board> boardList = query.getResultList();
+
+        // 이 쿼리문에서 게시글을 작성한 사용자만 중복없이 담김
+        int[] ids = boardList.stream().mapToInt(value -> value.getUser().getId()).distinct().toArray();
+        // select u from User u where b.id in (?, ?); --> 동적 in 쿼리
+        String q2 = "select u from User u where u.id in (";
+        for (int i = 0; i < ids.length; i++) {
+            if (i == ids.length - 1) {
+                q2 = q2 + ids[i] + ")";
+            } else {
+                q2 = q2 + ids[i] + ",";
+            }
+        }
+        // 여기서 동적 인쿼리가 완성되어 발송, 그 객체들을 userList로 받는다.
+        List<User> userList = em.createQuery(q2, User.class).getResultList();
+        for (Board board : boardList){
+            for (User user : userList){
+                if (board.getUser().getId() == user.getId()){
+                    board.setUser(user);
+                }
+            }
+        }
+    }
+
+
+    @Test
+    public void findAll_test() {
         // given
 
         // when
@@ -33,7 +64,7 @@ public class BoardRepositoryTest {
     }
 
     @Test
-    public void findById_test(){
+    public void findById_test() {
         // given
         int id = 1;
         // when
