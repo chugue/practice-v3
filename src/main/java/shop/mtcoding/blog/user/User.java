61944a65739b99b31a75097186b6405ee7dd67cd
chugue <chugue85@gmail.com>
2024-03-22 16:49:13 +0900

    user, board table ORM

--- src/main/java/shop/mtcoding/blog/user/User.java
+++ src/main/java/shop/mtcoding/blog/user/User.java
@@ -1,39 +1,36 @@
-package shop.mtcoding.blog.board;
+package shop.mtcoding.blog.user;
+
 
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import shop.mtcoding.blog.user.User;
 
 import java.sql.Timestamp;
 
 @NoArgsConstructor
+@Table (name = "user_tb")
 @Data
-@Table(name = "board_tb")
 @Entity
-public class Board {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String title;
-    private String content;
+    @Column(unique = true)
     private String username;
+    private String password;
+    private String email;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User user;
-
-    @CreationTimestamp // pc -> db (날짜주입)
+    @CreationTimestamp
     private Timestamp createdAt;
 
     @Builder
-    public Board(Integer id, String title, String content, String username, User user, Timestamp createdAt) {
+    public User(Integer id, String username, String password, String email, Timestamp createdAt) {
         this.id = id;
-        this.title = title;
-        this.content = content;
         this.username = username;
-        this.user = user;
+        this.password = password;
+        this.email = email;
         this.createdAt = createdAt;
     }
 }
