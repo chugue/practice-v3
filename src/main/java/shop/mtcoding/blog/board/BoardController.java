c7d570c03a9183a31e6bf2dc31b5c3ff73cd9802
AI_Nomads <chugue85@gmail.com>
2024-03-23 10:41:18 +0900

    save done

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,11 +1,13 @@
 package shop.mtcoding.blog.board;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import shop.mtcoding.blog.user.User;
 
 import java.util.List;
 
@@ -14,6 +16,7 @@ import java.util.List;
 @Controller
 public class BoardController {
     private final BoardRepository boardRepository;
+    private final HttpSession session;
 
     @PostMapping("/board/{id}/update")
     public String update(@PathVariable Integer id){
@@ -31,7 +34,10 @@ public class BoardController {
     }
 
     @PostMapping("/board/save")
-    public String save(){
+    public String save(BoardRequest.SaveDTO reqDTO){
+        User sessionUser = (User)session.getAttribute("sessionUser");
+        boardRepository.save(reqDTO.toEntity(sessionUser));
+
         return "redirect:/";
     }
 
@@ -43,7 +49,9 @@ public class BoardController {
     }
 
     @GetMapping("/board/save-form")
-    public String saveForm() {
+    public String saveForm(HttpServletRequest request) {
+        User user = (User) session.getAttribute("sessionUser");
+        request.setAttribute("user", user);
         return "board/save-form";
     }
 
